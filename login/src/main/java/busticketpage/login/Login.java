7c2c81b5a9b95c3a28c1c220552bf688819d89ed
palani-pt4724 <palani-pt4724@palani-pt4724.tsi.zohocorpin.com>
2022-01-21 09:45:27 +0530

    demo

---
+++ login/src/main/java/busticketpage/login/Login.java
@@ -0,0 +1,55 @@
+package busticketpage.login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import busticketlogin.bean.LoginBean;
+import busticketlogin.database.LoginDao;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		LoginBean loginbean = new LoginBean();
+		loginbean.setUsername(username);
+		loginbean.setPassword(password);
+		
+		LoginDao loginDao = new LoginDao();
+		if(loginDao.validate(loginbean)){
+			response.sendRedirect("loginSucess.jsp");
+		}
+		else {
+			response.sendRedirect("login.jsp");
+		}
+	}
+
+}
